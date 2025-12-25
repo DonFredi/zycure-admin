915acb56ea9c3d09f572e7198b1d0483f3be749e
DonFredi <dondadi.fn@gmail.com>
2025-12-25 13:40:55 +0300

    commitment

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -27,7 +27,7 @@ export async function GET(
 }
 
 export async function PUT(req: NextRequest, context: { params: { id: string } }) {
-  const { id } = context.params;
+  const { id } = await context.params;
   const { status } = await req.json();
 
   if (!status) {
